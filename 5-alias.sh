54662b9d700f4c1d1c6eaf7df575d4b5d97448de
Henrique Custódio <henriqueffc17@gmail.com>
2022-02-23 00:43:02 -0300

    Update 5-alias.sh

--- 5-alias.sh
+++ 5-alias.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Alias
-alias neo="neofetch"
-alias atualizar="flatpak update && flatpak remove --unused && yay -Syu"
-alias limpar="sudo paccache -rk1 && pacman -Qdtq && yay -Yc"
-alias dados="yay -Ps"
-alias reflector="sudo reflector -l 10 -c Brazil -a 12 -p https -p http --sort rate --save /etc/pacman.d/mirrorlist && sudo pacman -Syy"
-alias xls="exa -a --icons --color=always --group-directories-first"
-alias xll="exa -lag --icons --color=always --group-directories-first --octal-permissions"
-alias bat="bat --theme=Dracula"
+mv .bash_aliases ~/
+echo "source ~/.bash_aliases" >> ~/.bashrc
