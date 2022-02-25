7e9b6d241eb47bd8511a6425f876d927c038ff47
Henrique Custódio <henriqueffc17@gmail.com>
2022-02-25 17:40:23 -0300

    Update 1-videoeaudio.sh

--- 1-videoeaudio.sh
+++ 1-videoeaudio.sh
@@ -6,11 +6,23 @@ sudo rm -r /archpost-installation
 #Refresh database
 sudo pacman -Syu
 
-# Reflector
-sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-sudo pacman -S --needed --noconfirm reflector rsync
-sudo reflector -c Brazil -a 12 --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -Syyu
+#Reflector
+echo -n "Voc\u00ea quer executar o reflector para atualizar o mirrorlist? (S) sim / (N) n\u00e3o "
+read resposta
+case "$resposta" in
+     s|S|"")
+         sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+         sudo pacman -S --needed --noconfirm reflector rsync
+         sudo reflector -c Brazil -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+         sudo pacman -Syyu
+     ;;
+     n|N)
+         echo "Continuando a instala\u00e7\u00e3o dos pacotes"
+     ;;
+     *)
+         echo "Op\u00e7\u00e3o inv\u00e1lida"
+     ;;
+esac
 
 # Video (Intel e Nvidia)
 sudo pacman -S --needed nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings opencl-nvidia vulkan-icd-loader libvdpau-va-gl libva-vdpau-driver libvdpau vulkan-tools ocl-icd mesa mesa-vdpau libva vdpauinfo libva-utils intel-ucode intel-media-sdk lib32-mesa vulkan-intel intel-media-driver intel-compute-runtime intel-graphics-compiler nvidia-prime clinfo nvtop
@@ -22,12 +34,24 @@ sudo pacman -S --needed vlc ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins
 sudo pacman -S --needed pipewire pipewire-alsa pipewire-jack wireplumber pipewire-pulse gst-plugin-pipewire libpulse pipewire-x11-bell xdg-desktop-portal
 
 #Apparmor
-sudo pacman -S --needed apparmor python-notify2 python-psutil 
-sudo systemctl enable apparmor.service
-sudo touch /var/log/syslog
-mkdir ~/.config/autostart
-mv apparmor-notify.desktop ~/.config/autostart
-sudo sed -i '34s/#//' /etc/apparmor/parser.conf
-sudo chown henriqueffc:henriqueffc ~/.config/autostart
+echo -n "Voc\u00ea quer instalar o Apparmor? (S) sim / (N) n\u00e3o "
+read resposta
+case "$resposta" in
+     s|S|"")
+        sudo pacman -S --needed apparmor python-notify2 python-psutil 
+        sudo systemctl enable apparmor.service
+        sudo touch /var/log/syslog
+        mkdir ~/.config/autostart
+        mv apparmor-notify.desktop ~/.config/autostart
+        sudo sed -i '34s/#//' /etc/apparmor/parser.conf
+        sudo chown henriqueffc:henriqueffc ~/.config/autostart
+     ;;
+     n|N)
+         echo "Continuando a instala\u00e7\u00e3o dos pacotes"
+     ;;
+     *)
+         echo "Op\u00e7\u00e3o inv\u00e1lida"
+     ;;
+esac
 
-printf "\e[1;32mFim! Acrescente as instruções contidas em paBoot.txt nos parâmetros do boot. Reinicie o sistema.\e[0m"
+printf "\e[1;32mFim! Caso tenha instalado o AppArmor acrescente as instruções do arquivo -paBoot.txt- nos parâmetros do boot e depois reinicie o sistema. Se você não instalou o Apparmor apenas proceda com a reinicialização do sistema\e[0m"
