4606a793751e96fbc1159602d08ae61b1d6f57f7
Henrique Custódio <henriqueffc17@gmail.com>
2022-02-25 18:25:04 -0300

    Update 3-flatpak.sh

--- 3-flatpak.sh
+++ 3-flatpak.sh
@@ -26,16 +26,16 @@ flatpak install flathub com.mattjakeman.ExtensionManager -y
 flatpak install flathub org.gtk.Gtk3theme.Adwaita-dark -y
 
 # Flatpak Remote-Beta
-echo -n "Voc\u00ea quer adicionar o remote Flathub Beta? (S) sim / (N) n\u00e3o "
+echo -n "Você quer adicionar o remote Flathub Beta? (S) sim / (N) não "
 read resposta
 case "$resposta" in
      s|S|"")
       flatpak remote-add flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
      ;;
      n|N)
-         echo "Continuando a instala\u00e7\u00e3o dos pacotes"
+         echo "Continuando a instalação dos pacotes"
      ;;
      *)
-         echo "Op\u00e7\u00e3o inv\u00e1lida"
+         echo "Opção inválida"
      ;;
 esac
