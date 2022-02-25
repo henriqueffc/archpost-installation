fc52f54bd1ab09c89efd8b566da9482760bcb7ec
Henrique Custódio <henriqueffc17@gmail.com>
2022-02-25 18:27:28 -0300

    Update 3-flatpak.sh

--- 3-flatpak.sh
+++ 3-flatpak.sh
@@ -33,7 +33,7 @@ case "$resposta" in
       flatpak remote-add flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
      ;;
      n|N)
-         echo "Fim da instalaç"
+         echo "Fim da instalação"
      ;;
      *)
          echo "Opção inválida"
