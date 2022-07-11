20b31d69faf609ad002c15dea646658e54a9c4fd
Henrique Custódio <henriqueffc17@gmail.com>
2022-07-11 17:00:24 -0300

    Update 4-flatpak-e-fonte.sh

--- 4-flatpak-e-fonte.sh
+++ 4-flatpak-e-fonte.sh
@@ -70,6 +70,7 @@ flatpak install flathub com.github.micahflee.torbrowser-launcher -y
 flatpak install flathub org.zotero.Zotero -y
 flatpak install flathub com.usebottles.bottles -y
 flatpak install flathub com.github.weclaw1.ImageRoll -y
+flatpak install flathub org.gnome.Games -y
 
 # Flatpak Remote-Beta
 
