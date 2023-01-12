97043bcf7a7b4fdd39c169b7122356ceea9335fe
Henrique Custódio <henriqueffc17@gmail.com>
2023-01-12 01:29:37 -0300

    Update 8-zsh-ohmyzsh.sh

--- 8-zsh-ohmyzsh.sh
+++ 8-zsh-ohmyzsh.sh
@@ -98,7 +98,7 @@ else
 			git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 			git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
 			cp ~/.zshrc ~/.zshrc.bak
-			sed -i 's/plugins=(git)/\plugins=(git zsh-autosuggestions zsh-syntax-highlighting colored-man-pages command-not-found thefuck copyfile copybuffer dirhistory web-search copypath history-substring-search)/' ~/.zshrc
+			sed -i 's/plugins=(git)/\plugins=(git zsh-autosuggestions zsh-syntax-highlighting colored-man-pages command-not-found universalarchive thefuck copyfile copybuffer dirhistory web-search copypath history-substring-search)/' ~/.zshrc
 			sed -i 's|robbyrussell|powerlevel10k/powerlevel10k|' ~/.zshrc
 			sed -i '2 r./zsh/dracula.txt' ~/.zshrc
 			linenumber=$(grep -nr 'source' ~/.zshrc | gawk '{print $1}' FS=":")
