c0e376762fa7e0d8f228de0080764e954c49ae15
Henrique Custódio <henriqueffc17@gmail.com>
2023-02-13 10:59:03 -0300

    Update 7-extensions.sh

--- 7-extensions.sh
+++ 7-extensions.sh
@@ -11,13 +11,15 @@ AZUL='\e[1;34m'
 VERDE='\e[1;32m'
 FIM='\e[0m'
 
-sudo pacman -S aria2 libgtop lm_sensors git bluez-utils --needed --noconfirm
+sudo pacman -S aria2 libgtop lm_sensors git bluez-utils gettext unzip ninja meson jq eslint --needed --noconfirm
 
 # Instalação das extensões
 
 echo -e "$AZUL AppIndicator and KStatusNotifierItem Support $FIM" 
 aria2c https://github.com/ubuntu/gnome-shell-extension-appindicator/archive/refs/tags/v46.zip
-gnome-extensions install --force 'gnome-shell-extension-appindicator-46.zip'
+unzip gnome-shell-extension-appindicator-46.zip
+meson setup ./gnome-shell-extension-appindicator-46 /tmp/g-s-appindicators-build
+ninja -C /tmp/g-s-appindicators-build install
 
 echo -e "$AZUL Clipboard History $FIM" 
 aria2c https://github.com/SUPERCILEX/gnome-clipboard-history/archive/refs/tags/1.3.0.zip
@@ -36,5 +38,17 @@ make -C $HOME/.local/share/gnome-shell/extensions/bluetooth-quick-connect@bjaros
 echo -e "$AZUL Vitals $FIM" 
 aria2c https://github.com/corecoding/Vitals/releases/download/v59.0.6/vitals.zip
 gnome-extensions install --force 'vitals.zip'
+
+echo -e "$AZUL Just Perfection $FIM"
+aria2c https://gitlab.gnome.org/jrahmatzadeh/just-perfection/-/archive/23.0/just-perfection-23.0.zip
+unzip just-perfection-23.0.zip
+./just-perfection-23.0/scripts/build.sh
+gnome-extensions install --force './just-perfection-23.0/just-perfection-desktop@just-perfection.shell-extension.zip'
+
+echo -e "$AZUL Blur My Shell $FIM"
+wget https://github.com/aunetx/blur-my-shell/releases/download/v44/blur-my-shell@aunetx.shell-extension.zip
+gnome-extensions install --force 'blur-my-shell@aunetx.shell-extension.zip'
+
+sudo pacman -R ninja meson jq eslint
 
 printf "%s $VERDE Fim! Reinicie com o comando reboot e habilite as extensões usando o app Extensões após a reinicialização do sistema $FIM \n"
