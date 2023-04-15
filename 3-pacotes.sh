6e894aff312ead05c89959af4ca8eb4f60229598
Henrique Custódio <henriqueffc17@gmail.com>
2023-04-15 00:54:56 -0300

    retirada do Joplin

--- 3-pacotes.sh
+++ 3-pacotes.sh
@@ -102,9 +102,6 @@ mkdir /home/$USER/Applications
 mv ~/Downloads/*.AppImage /home/$USER/Applications
 chmod +x /home/$USER/Applications/*.AppImage
 
-# Joplin
-wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
-
 # Fontes
 sudo mkdir -p /usr/local/share/fonts
 tar -xf ./pacotes/fonts.tar.xz -C ~/Downloads
