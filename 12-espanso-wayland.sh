9102984932bbb1afeedfdaff54c25a735a26af20
Henrique Custódio <henriqueffc17@gmail.com>
2023-05-09 19:51:26 -0300

    Alterações na estrutura do script

--- 12-espanso-wayland.sh
+++ 12-espanso-wayland.sh
@@ -11,16 +11,11 @@ yay espanso-wayland
 
 # Inicialização do Espanso
 
-espanso service start --unmanaged
-
-# Execução do Espanso 20 segundos após inicialização do Gnome. Não será usada a opção padrão pelo systemd. Pois, quando usada, na inicialização do Gnome ela apresenta um irritante ícone no centro da tela. Prefiro usar o método abaixo.
-
-mv ./espanso/espanso.desktop $HOME/.config/autostart
+espanso service register
 
 # Mudança do laytout do teclado para br
 
 echo -e "\n keyboard_layout:" >>$HOME/.config/espanso/config/default.yml
-echo -e "  { layout: br }" >>$HOME/.config/espanso/config/default.yml
-
-# Reinicialização do Espanso
-espanso service start --unmanaged
+echo "  { layout: br }" >>$HOME/.config/espanso/config/default.yml
+echo " search_shortcut: ALT+SHIFT+SPACE" >>$HOME/.config/espanso/config/default.yml
+echo " show_notifications: false" >>$HOME/.config/espanso/config/default.yml
