e49436b73d8f8650c66bc744e83b023909548d30
Henrique Custódio <henriqueffc17@gmail.com>
2023-05-31 16:59:54 -0300

    mudanças nos favoritos da dock

--- 6-grid.sh
+++ 6-grid.sh
@@ -34,7 +34,7 @@ gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folder
 gsettings set org.gnome.desktop.app-folders folder-children "['Utilities', 'YaST', 'Office', 'Programas', 'Jogos', 'Nvidia', 'Podman', 'GNOME', 'Internet', 'Multimídia']"
 
 # Dock
-gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'Alacritty.desktop', 'obsidian.desktop', 'org.gnome.Nautilus.desktop', 'code.desktop']"
+gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'Alacritty.desktop', 'obsidian.desktop', 'code.desktop', 'org.gnome.Nautilus.desktop']"
 
 # Nautilus Bookmarks
 mkdir ~/ToDo/
