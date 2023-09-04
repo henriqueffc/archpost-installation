dd0cbfdfda6846f5b4d48f6b5e18c7e94a159076
Henrique Custódio <henriqueffc17@gmail.com>
2023-09-04 19:46:01 -0300

    adição do pacote silicon

--- 10-lunarvim.sh
+++ 10-lunarvim.sh
@@ -22,13 +22,13 @@ while :; do
        read -r resposta
        case "$resposta" in
        s | S | "")
-              sudo pacman -S git neovim base-devel unzip curl wget fd ripgrep ripgrep-all make python npm nodejs rust lazygit wl-clipboard xsel ttf-jetbrains-mono-nerd --needed
+              sudo pacman -S git neovim base-devel unzip curl wget fd ripgrep ripgrep-all make python npm nodejs rust lazygit silicon wl-clipboard xsel ttf-jetbrains-mono-nerd --needed
               LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
               echo -e "$VERDE Fim da instalação. $FIM"
               break
               ;;
        n | N)
-              sudo pacman -S git base-devel unzip curl wget fd ripgrep ripgrep-all make python npm nodejs rust lazygit wl-clipboard xsel ttf-jetbrains-mono-nerd --needed
+              sudo pacman -S git base-devel unzip curl wget fd ripgrep ripgrep-all make python npm nodejs rust lazygit silicon wl-clipboard xsel ttf-jetbrains-mono-nerd --needed
               yay neovim-nightly-bin
               bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
               echo -e "$VERDE Fim da instalação. $FIM"
