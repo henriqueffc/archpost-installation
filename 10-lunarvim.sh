ae012de73a779c72c241abf137312cc8ead67217
Henrique Custódio <henriqueffc17@gmail.com>
2023-10-16 15:22:04 -0300

    alterações no PATH e nos pacotes de instalação

--- 10-lunarvim.sh
+++ 10-lunarvim.sh
@@ -12,7 +12,7 @@ FIM='\e[0m'
 
 # Paths
 cp $HOME/.zshrc $HOME/.zshrc.bak1
-echo 'export PATH="$HOME/.local/bin:$PATH"' >>$HOME/.zshrc
+#echo 'export PATH="$HOME/.local/bin:$PATH"' >>$HOME/.zshrc ## configurado no script nº 8
 echo 'export PATH="$HOME/.cargo/bin:$PATH"' >>$HOME/.zshrc
 echo 'export npm_config_prefix="$HOME/.local"' >>$HOME/.zshrc
 source $HOME/.zshrc
@@ -22,13 +22,13 @@ while :; do
        read -r resposta
        case "$resposta" in
        s | S | "")
-              sudo pacman -S git neovim base-devel unzip curl wget fd ripgrep ripgrep-all make python npm nodejs rust lazygit silicon wl-clipboard xsel ttf-jetbrains-mono-nerd --needed
+              sudo pacman -S git neovim base-devel unzip curl wget fd ripgrep ripgrep-all make python python-pipenv python-pipx npm nodejs rust lazygit silicon wl-clipboard xsel ttf-jetbrains-mono-nerd --needed
               LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
               echo -e "$VERDE Fim da instalação. $FIM"
               break
               ;;
        n | N)
-              sudo pacman -S git base-devel unzip curl wget fd ripgrep ripgrep-all make python npm nodejs rust lazygit silicon wl-clipboard xsel ttf-jetbrains-mono-nerd --needed
+              sudo pacman -S git base-devel unzip curl wget fd ripgrep ripgrep-all make python python-pipenv python-pipx npm nodejs rust lazygit silicon wl-clipboard xsel ttf-jetbrains-mono-nerd --needed
               yay neovim-nightly-bin
               bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
               echo -e "$VERDE Fim da instalação. $FIM"
