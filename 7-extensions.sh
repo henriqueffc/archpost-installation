813eba7281a3e82071e5b49e0206fb7b207aed4f
Henrique Custódio <henriqueffc17@gmail.com>
2023-10-17 16:06:33 -0300

    retirada da extensão AppIndicator and KStatusNotifierItem Support

--- 7-extensions.sh
+++ 7-extensions.sh
@@ -11,16 +11,9 @@ AZUL='\e[1;34m'
 VERDE='\e[1;32m'
 FIM='\e[0m'
 
-sudo pacman -S aria2 libappindicator-gtk3 libgtop lm_sensors git gettext unzip ninja meson jq eslint --needed --noconfirm
+sudo pacman -S aria2 libgtop lm_sensors git --needed --noconfirm
 
 # Instalação das extensões
-
-vap="57"
-echo -e "$AZUL AppIndicator and KStatusNotifierItem Support $FIM"
-aria2c https://github.com/ubuntu/gnome-shell-extension-appindicator/archive/refs/tags/v$vap.zip
-unzip gnome-shell-extension-appindicator-$vap.zip
-meson setup ./gnome-shell-extension-appindicator-$vap /tmp/g-s-appindicators-build
-ninja -C /tmp/g-s-appindicators-build install
 
 vvit="63.0.0"
 echo -e "$AZUL Vitals $FIM" 
@@ -34,7 +27,5 @@ gnome-extensions install --force 'no-overview.zip'
 
 echo -e "$AZUL Clipboard Indicator $FIM"
 git clone https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator.git ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
-
-sudo pacman -Rn ninja meson eslint jq
 
 printf "%s $VERDE Fim! Reinicie com o comando reboot e habilite as extensões usando o app Extensões após a reinicialização do sistema $FIM \n"
