04c07e6947655e3ea8ba34de02d75fa69898c63b
Henrique Custódio <henriqueffc17@gmail.com>
2023-11-17 15:19:01 -0300

    retirada de alguns apps

--- 4-flatpak-e-mc.sh
+++ 4-flatpak-e-mc.sh
@@ -23,7 +23,6 @@ sudo pacman -S flatpak --needed --noconfirm
 
 # Flatpak Aplicativos
 flatpak install flathub org.gimp.GIMP -y
-flatpak install flathub com.calibre_ebook.calibre -y
 flatpak install flathub net.davidotek.pupgui2 -y
 flatpak install flathub io.gitlab.adhami3310.Converter -y
 flatpak install flathub org.gtkhash.gtkhash -y
@@ -34,14 +33,11 @@ flatpak install flathub com.skype.Client -y
 flatpak install flathub com.github.unrud.VideoDownloader -y
 flatpak install flathub fr.romainvigier.MetadataCleaner -y
 flatpak install flathub org.gnome.gitlab.YaLTeR.VideoTrimmer -y
-flatpak install flathub us.zoom.Zoom -y
 flatpak install flathub com.belmoussaoui.Obfuscate -y
 flatpak install flathub com.github.wwmm.easyeffects -y
 flatpak install flathub com.mattjakeman.ExtensionManager -y
 flatpak install flathub org.gtk.Gtk3theme.Adwaita-dark -y
 flatpak install flathub com.github.huluti.Curtail -y
-flatpak install flathub de.haeckerfelix.Shortwave -y
-flatpak install flathub org.zotero.Zotero -y
 flatpak install flathub com.usebottles.bottles -y
 flatpak install flathub io.github.nokse22.teleprompter -y
 flatpak install flathub io.podman_desktop.PodmanDesktop -y
@@ -55,26 +51,27 @@ flatpak install flathub org.gnome.World.PikaBackup -y
 flatpak install flathub io.bassi.Amberol -y
 flatpak install flathub io.github.flattool.Warehouse -y
 flatpak install flathub com.github.finefindus.eyedropper -y
+flatpak install flathub com.github.johnfactotum.Foliate -y
 
 # Flatpak Remote-Beta
 
 while :; do
-       echo -ne "$VERDE Você quer adicionar o remote Flathub Beta? $FIM $LVERDE (S) sim / (N) não $FIM"
-       read -r resposta
-       case "$resposta" in
-       s | S | "")
-              flatpak remote-add flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
-              echo -e "$VERDE Fim da instalação. $FIM"
-              break
-              ;;
-       n | N)
-              echo -e "$VERDE Fim da instalação. $FIM"
-              break
-              ;;
-       *)
-              echo -e "$RED Opção inválida. Responda a pergunta. $FIM"
-              ;;
-       esac
+	echo -ne "$VERDE Você quer adicionar o remote Flathub Beta? $FIM $LVERDE (S) sim / (N) não $FIM"
+	read -r resposta
+	case "$resposta" in
+	s | S | "")
+		flatpak remote-add flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+		echo -e "$VERDE Fim da instalação. $FIM"
+		break
+		;;
+	n | N)
+		echo -e "$VERDE Fim da instalação. $FIM"
+		break
+		;;
+	*)
+		echo -e "$RED Opção inválida. Responda a pergunta. $FIM"
+		;;
+	esac
 done
 
 echo -e "$AZUL
