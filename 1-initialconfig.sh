f1d1560e5352c035ddd2cf30d8f335a2af6ac7b9
Henrique Custódio <henriqueffc17@gmail.com>
2024-01-11 00:45:49 -0300

    novas variáveis para o vulkan usado pelo sistema

--- 1-initialconfig.sh
+++ 1-initialconfig.sh
@@ -53,6 +53,8 @@ echo 'mesa_glthread=true' >>/etc/environment
 echo 'FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border"' >>/etc/environment
 echo 'FZF_DEFAULT_COMMAND="fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"' >>/etc/environment
 echo 'GST_PLUGIN_FEATURE_RANK=qsvvp9dec:MAX,qsvh264dec:MAX,qsvh265dec:MAX,qsvjpegdec:MAX,vavp9dec:MAX,vavp8dec:MAX,vah264dec:MAX,vah265dec:MAX,vampeg2dec:MAX,vajpegdec:MAX' >>/etc/environment
+echo 'ANV_VIDEO_DECODE=1' >>/etc/environment
+echo 'VK_DRIVER_FILES=/usr/share/vulkan/icd.d/intel_icd.x86_64.json' >>/etc/environment
 
 # PC speaker - turn off beep shutdown
 echo 'blacklist pcspkr' >/etc/modprobe.d/nobeep.conf
