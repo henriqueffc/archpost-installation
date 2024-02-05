aea179c3282013b4b67b2143dfe93d77894c2171
Henrique Custódio <henriqueffc17@gmail.com>
2024-02-05 18:14:59 -0300

    retirada do Upscayl versão flatpak

--- 4-flatpak-e-mc.sh
+++ 4-flatpak-e-mc.sh
@@ -50,7 +50,6 @@ flatpak install flathub io.bassi.Amberol -y
 flatpak install flathub io.github.flattool.Warehouse -y
 flatpak install flathub com.github.finefindus.eyedropper -y
 flatpak install flathub com.github.johnfactotum.Foliate -y
-flatpak install flathub org.upscayl.Upscayl -y
 flatpak install flathub fr.handbrake.ghb -y
 flatpak install flathub fr.handbrake.ghb.Plugin.IntelMediaSDK -y
 flatpak install flathub app.drey.Dialect -y
