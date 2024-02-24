27b0e7bf9f7b1baf3ff7aecb5eef44500a742f03
Henrique Custódio <henriqueffc17@gmail.com>
2024-02-23 23:15:29 -0300

    nova variável para aplicativos electron versão 28

--- 1-initialconfig.sh
+++ 1-initialconfig.sh
@@ -56,6 +56,7 @@ echo 'GST_PLUGIN_FEATURE_RANK=vah264dec:MAX,vah265dec:MAX,vavp9dec:MAX,vavp8dec:
 echo 'ANV_VIDEO_DECODE=1' >>/etc/environment
 echo 'VK_DRIVER_FILES=/usr/share/vulkan/icd.d/intel_icd.x86_64.json' >>/etc/environment
 echo '__EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/50_mesa.json:/usr/share/glvnd/egl_vendor.d/10_nvidia.json' >>/etc/environment
+echo 'ELECTRON_OZONE_PLATFORM_HINT=wayland' >>/etc/environment
 
 # PC speaker - turn off beep shutdown
 echo 'blacklist pcspkr' >/etc/modprobe.d/nobeep.conf
