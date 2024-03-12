eb5c231aa46190112bacb67c1d667a150b75954f
Henrique Custódio <henriqueffc17@gmail.com>
2024-03-12 12:24:43 -0300

    retirada dos módulos da nvidia do mkinitcpio.conf

--- 1-initialconfig.sh
+++ 1-initialconfig.sh
@@ -90,9 +90,8 @@ sed -i 's/-march=x86-64 -mtune=generic/-march=native/g' /etc/makepkg.conf
 sed -i 's/#RUSTFLAGS=.*/RUSTFLAGS="-C opt-level=2 -C target-cpu=native"/g' /etc/makepkg.conf
 sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -z --threads=0 -)/g' /etc/makepkg.conf
 
-# Intel - i915 / HOOKS / mkinitcpio.conf
+# HOOKS / mkinitcpio.conf
 cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak
-sed -i 's/MODULES=.*/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/g' /etc/mkinitcpio.conf
 sed -i 's/HOOKS=.*/HOOKS=(systemd autodetect microcode modconf kms keyboard sd-vconsole block filesystems fsck)/g' /etc/mkinitcpio.conf
 sed -i 's/#MODULES_DECOMPRESS="yes"/MODULES_DECOMPRESS="yes"/g' /etc/mkinitcpio.conf
 mkinitcpio -P
