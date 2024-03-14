c7f68c7b5c2f660d48c5d47b92baa29dff21cb28
Henrique Custódio <henriqueffc17@gmail.com>
2024-03-14 16:34:46 -0300

    adição dos pacotes clang e shellcheck

--- 9-helix.sh
+++ 9-helix.sh
@@ -6,7 +6,7 @@
 # License: MIT License
 
 # Instalação
-sudo pacman -Syu helix prettier pyright shfmt bash-language-server vscode-css-languageserver lua-language-server yaml-language-server typescript-language-server marksman deno python-black vscode-html-languageserver go gopls go-tools taplo-cli rust python python-pipenv python-pip python-pipx npm nodejs aria2 bash-completion gdu lazygit fzf ripgrep ripgrep-all git base-devel unzip curl wget fd gawk eza procs sd openssh trash-cli bat bat-extras strace man-pages man-pages-pt_br wl-clipboard silicon xsel ttf-jetbrains-mono-nerd --needed
+sudo pacman -Syu helix clang shellcheck prettier pyright shfmt bash-language-server vscode-css-languageserver lua-language-server yaml-language-server typescript-language-server marksman deno python-black vscode-html-languageserver go gopls go-tools taplo-cli rust python python-pipenv python-pip python-pipx npm nodejs aria2 bash-completion gdu lazygit fzf ripgrep ripgrep-all git base-devel unzip curl wget fd gawk eza procs sd openssh trash-cli bat bat-extras strace man-pages man-pages-pt_br wl-clipboard silicon xsel ttf-jetbrains-mono-nerd --needed
 
 # Paths
 cp $HOME/.zshrc $HOME/.zshrc.bak1
