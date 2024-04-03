4e4bf0ad4db429b0043eabb6106a7f28e541accc
Henrique Custódio <henriqueffc17@gmail.com>
2024-04-03 18:03:47 -0300

    substituição do backend da gpu
    Vulkan -> Gl

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -41,7 +41,7 @@ config.initial_cols = 100
 config.initial_rows = 25
 
 for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
-  if gpu.backend == 'Vulkan' and gpu.device_type == 'IntegratedGpu' then
+  if gpu.backend == 'Gl' and gpu.device_type == 'IntegratedGpu' then
     config.webgpu_preferred_adapter = gpu
     config.front_end = 'WebGpu'
     break
