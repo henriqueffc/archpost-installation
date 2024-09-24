89723cf6866e6da805a547eef7a2c3b5e0982648
Henrique Custódio <henriqueffc17@gmail.com>
2024-09-24 12:25:37 -0300

    modificações para melhorias na parte gráfica

--- 1-initialconfig.sh
+++ 1-initialconfig.sh
@@ -51,7 +51,7 @@ echo 'FZF_DEFAULT_COMMAND="fd --type f --hidden --exclude .git"' >>/etc/environm
 echo 'GST_PLUGIN_FEATURE_RANK=vah264dec:MAX,vah265dec:MAX,vavp9dec:MAX,vavp8dec:MAX,vampeg2dec:MAX,vajpegdec:MAX,vaav1dec:MAX' >>/etc/environment
 echo 'ANV_VIDEO_DECODE=1' >>/etc/environment
 echo 'ELECTRON_OZONE_PLATFORM_HINT=wayland' >>/etc/environment
-echo 'CLUTTER_PAINT=disable-dynamic-max-render-time' >>/etc/environment
+echo 'GSK_RENDERER=vulkan' >>/etc/environment
 
 # PC speaker - turn off beep shutdown
 echo -e 'blacklist pcspkr' >/etc/modprobe.d/blacklist.conf
