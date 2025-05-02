13d7a2b6207526cb9e934fbd738bd5cdb5eb6e87
Henrique Custódio <henriqueffc17@gmail.com>
2025-05-02 16:26:03 -0300

    adição de pacotes
    uv,pyenv,python-pipenv,libxml2-legacy (esse último pacote é, até o
    momento, necessário para o Microsoft Edge -
    https://aur.archlinux.org/packages/microsoft-edge-stable-bin#comment-1022200)

--- 9-helix.sh
+++ 9-helix.sh
@@ -6,7 +6,7 @@
 # License: MIT License
 
 # Instalação
-sudo pacman -Syu helix clang shellcheck stylua libxml2 yamlfmt yamllint pyright shfmt bash-language-server vscode-css-languageserver lua-language-server typescript-language-server marksman deno python-black vscode-html-languageserver go gopls go-tools taplo-cli rust python python-pipenv python-pip python-pipx npm nodejs aria2 bash-completion gdu lazygit fzf ripgrep ripgrep-all git base-devel unzip curl wget fd gawk eza procs sd openssh trash-cli bat bat-extras strace man-pages man-pages-pt_br wl-clipboard silicon xsel ttf-jetbrains-mono-nerd vscode-json-languageserver rust-analyzer ansible-language-server ansible-lint --needed
+sudo pacman -Syu helix clang shellcheck stylua libxml2 yamlfmt yamllint pyright shfmt bash-language-server vscode-css-languageserver lua-language-server typescript-language-server marksman deno python-black vscode-html-languageserver go gopls go-tools taplo-cli rust python pyenv python-pipenv uv python-pip python-pipx npm nodejs aria2 bash-completion gdu lazygit fzf ripgrep ripgrep-all git base-devel unzip curl wget fd gawk eza procs sd openssh trash-cli bat bat-extras strace man-pages man-pages-pt_br wl-clipboard silicon xsel ttf-jetbrains-mono-nerd vscode-json-languageserver rust-analyzer ansible-language-server ansible-lint --needed
 
 # Paths
 cp $HOME/.zshrc $HOME/.zshrc.bak1
