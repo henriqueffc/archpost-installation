d13cbd4c84f317678d8e1ffad44bac910d542e4d
Henrique Custódio <henriqueffc17@gmail.com>
2025-05-17 18:25:08 -0300

    alteração no modo de configurar o grid

--- 6-grid.sh
+++ 6-grid.sh
@@ -6,32 +6,32 @@
 # License: MIT License
 
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Office/ name "Office"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Office/ apps "['libreoffice-writer.desktop', 'libreoffice-calc.desktop', 'garden.jamie.Morphosis.desktop', 'info.febvre.Komikku.desktop', 'gimagereader-gtk.desktop', 'com.github.xournalpp.xournalpp.desktop', 'io.github.nokse22.teleprompter.desktop', 'com.github.dynobo.normcap.desktop', 'com.github.jeromerobert.pdfarranger.desktop', 'com.github.johnfactotum.Foliate.desktop', 'app.drey.Dialect.desktop', 'org.gnome.gitlab.somas.Apostrophe.desktop', 'libreoffice-draw.desktop', 'libreoffice-impress.desktop', 'libreoffice-base.desktop', 'libreoffice-math.desktop', 'libreoffice-startcenter.desktop']"
+dconf load /org/gnome/desktop/app-folders/folders/Office/ <./grid/office.txt
 
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Programas/ name "Programas"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Programas/ apps "['org.gnome.World.PikaBackup.desktop', 'app.drey.KeyRack.desktop', 'it.mijorus.gearlever.desktop', 'org.gnome.Firmware.desktop', 'nl.emphisia.icon.desktop', 'firewall-config.desktop', 'veracrypt.desktop', 'com.github.finefindus.eyedropper.desktop', 'com.github.ADBeveridge.Raider.desktop', 'gparted.desktop', 'dev.geopjr.Collision.desktop', 'io.github.flattool.Warehouse.desktop', 'com.github.tchx84.Flatseal.desktop', 'com.mattjakeman.ExtensionManager.desktop', 'gnome-appfolders-manager.desktop', 'io.github.giantpinkrobots.varia.desktop', 'firejail-ui.desktop', 'firetools.desktop', 'alacarte.desktop', 'scrcpy.desktop', 'scrcpy-console.desktop']"
+dconf load /org/gnome/desktop/app-folders/folders/Programas/ <./grid/programas.txt
 
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Jogos/ name "Jogos"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Jogos/ apps "['steam.desktop', 'net.lutris.Lutris.desktop', 'io.github.benjamimgois.goverlay.desktop', 'com.vysp3r.ProtonPlus.desktop', 'winetricks.desktop']"
+dconf load /org/gnome/desktop/app-folders/folders/Jogos/ <./grid/jogos.txt
 
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/GNOME/ name "GNOME"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/GNOME/ apps "['org.gnome.Contacts.desktop', 'org.freedesktop.MalcontentControl.desktop', 'org.gnome.Maps.desktop', 'org.gnome.Weather.desktop', 'org.gnome.Boxes.desktop', 'org.gnome.Totem.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Extensions.desktop', 'org.gnome.Snapshot.desktop', 'org.gnome.tweaks.desktop', 'org.gnome.Epiphany.desktop', 'org.gnome.Music.desktop', 'org.gnome.seahorse.Application.desktop', 'org.gnome.GHex.desktop', 'org.gnome.Meld.desktop', 'org.gnome.font-viewer.desktop', 'yelp.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Console.desktop', 'org.gnome.Loupe.desktop', 'org.gnome.Logs.desktop', 'org.gnome.DiskUtility.desktop', 'org.gnome.Connections.desktop', 'org.gnome.Tour.desktop', 'org.gnome.baobab.desktop', 'org.gnome.Evince.desktop', 'org.gnome.Decibels.desktop', 'org.gnome.SimpleScan.desktop', 'org.gnome.FileRoller.desktop', 'org.gnome.SystemMonitor.desktop', 'org.gnome.Papers.desktop', 'org.gnome.Sysprof.desktop', 'ca.desrt.dconf-editor.desktop']"
+dconf load /org/gnome/desktop/app-folders/folders/GNOME/ <./grid/gnome.txt
 
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Internet/ name "Internet"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Internet/ apps "['weechat.desktop', 'im.dino.Dino.desktop', 'signal-desktop.desktop', 'firefox.desktop', 'com.discordapp.Discord.desktop', 'org.mozilla.Thunderbird.desktop', 'chromium.desktop']"
+dconf load /org/gnome/desktop/app-folders/folders/Internet/ <./grid/internet.txt
 
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Development/ name "Development"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Development/ apps "['com.ranfdev.DistroShelf.desktop', 'io.podman_desktop.PodmanDesktop.desktop', 'me.iepure.devtoolbox.desktop']"
+dconf load /org/gnome/desktop/app-folders/folders/Development/ <./grid/development.txt
 
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Nvidia/ name "Nvidia"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Nvidia/ apps "['nvidia-settings.desktop', 'nvtop.desktop']"
+dconf load /org/gnome/desktop/app-folders/folders/Nvidia/ <./grid/nvidia.txt
 
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Multimídia/ name "Multimídia"
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Multimídia/ apps "['io.gitlab.adhami3310.Footage.desktop', 'com.github.PintaProject.Pinta.desktop', 'org.kde.kdenlive.desktop', 'org.upscayl.Upscayl.desktop', 'audacity.desktop', 'com.obsproject.Studio.desktop', 'com.belmoussaoui.Obfuscate.desktop', 'fr.handbrake.ghb.desktop', 'org.nickvision.tubeconverter.desktop', 'com.github.wwmm.easyeffects.desktop', 'com.github.huluti.Curtail.desktop', 'mpv.desktop', 'org.pipewire.Helvum.desktop', 'com.saivert.pwvucontrol.desktop', 'io.github.chidiwilliams.Buzz.desktop', 'io.gitlab.adhami3310.Converter.desktop', 'org.gimp.GIMP.desktop']"
+dconf load /org/gnome/desktop/app-folders/folders/Multimídia/ <./grid/multimidia.txt
 
-gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Utilities/ apps "['gtk-lshw.desktop', 'albert.desktop', 'io.gitlab.adhami3310.Impression.desktop', 'yazi.desktop', 'com.dropbox.Client.desktop', 'gsmartcontrol.desktop', 'kdiskmark.desktop', 'zellij.desktop', 'btop.desktop', 'htop.desktop', 'bssh.desktop', 'bvnc.desktop', 'avahi-discover.desktop', 'qvidcap.desktop', 'vim.desktop', 'qv4l2.desktop', 'remote-viewer.desktop', 'lstopo.desktop', 'micro.desktop', 'kvantummanager.desktop', 'emoji-picker.desktop', 'cmake-gui.desktop', 'xterm.desktop', 'uxterm.desktop', 'xdvi.desktop', 'org.flameshot.Flameshot.desktop', 'electron34.desktop']"
+dconf load /org/gnome/desktop/app-folders/folders/Utilities/ <./grid/utilities.txt
 
-gsettings set org.gnome.desktop.app-folders folder-children "['Utilities', 'YaST', 'Office', 'Programas', 'Jogos', 'Nvidia', 'Development', 'GNOME', 'Internet', 'Multimídia']"
+gsettings set org.gnome.desktop.app-folders folder-children "['Utilities', 'Office', 'Programas', 'Jogos', 'Nvidia', 'Development', 'GNOME', 'Internet', 'Multimídia']"
 
 # Dock
 gsettings set org.gnome.shell favorite-apps "['com.mitchellh.ghostty.desktop', 'obsidian.desktop', 'microsoft-edge.desktop', 'org.telegram.desktop.desktop', 'org.gnome.Fractal.desktop', 'io.gitlab.news_flash.NewsFlash.desktop', 'com.jeffser.Alpaca.desktop', 'com.github.taiko2k.tauonmb.desktop', 'org.gnome.Nautilus.desktop']"
