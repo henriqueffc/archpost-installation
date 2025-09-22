51d6ff52d9599941cf154b3b84cfe52d176ca9b5
Henrique Custódio <henriqueffc17@gmail.com>
2025-09-22 16:42:08 -0300

    correções na instalação do zsh-completions

--- 7-zsh.sh
+++ 7-zsh.sh
@@ -15,11 +15,8 @@ FIM='\e[0m'
 
 # if ls -a "$HOME" | grep -oq .zshrc; then
 if test -f "/home/$USER/.zshrc"; then
-
     echo -e "$AZUL Zsh está habilitado. Continuando a instalação. $FIM" && sleep 2
-
 else
-
     while :; do
         echo -ne "$VERDE Você quer habilitar o ZSH? $FIM $RED Reiniciaremos a sessão após habilitar o ZSH. $FIM $VERDE Depois que logar novamente abra o terminal e escolha a opção 0 para criar o arquivo .zshrc. Após isso execute o script novamente. $FIM $LVERDE (S) sim / (N) não $FIM"
         read -r resposta
@@ -48,16 +45,19 @@ fi
 
 sudo pacman --needed -S - <./zsh/pacotes.txt
 
-cp ./zsh/zshrc.local ~/
+cp ./zsh/zshrc.local ~/.zshrc.local
+
+if test -f "/home/$USER/.functions"; then
+    echo -e "$AZUL Arquivo .functions já está no diretório home. Continuando a instalação. $FIM"
+else
+    cp ./aliases/functions ~/.functions
+fi
 
 mkdir -p ~/.zsh_plugins
 git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh_plugins/zsh-completions
-
 if test -f "/home/$USER/.zsh_plugins/zsh-completions/zsh-completions.plugin.zsh"; then
-    echo 'alias zsh_plugins="git -C $HOME/.zsh_plugins/zsh-completions pull"' >>~/.bash_aliases
+    echo 'alias zsh_plugins="git -C $HOME/.zsh_plugins/zsh-completions pull && rm -f ~/.zcompdump; compinit"' >>~/.bash_aliases
     echo 'fpath=(~/.zsh_plugins/zsh-completions/src $fpath)' >>~/.zshrc.local
-    rm -f ~/.zcompdump
-    compinit
 fi
 
 # oh-my-posh é instalado pelo script nº 5
@@ -65,4 +65,4 @@ if command -v oh-my-posh >/dev/null; then
     echo 'eval "$(oh-my-posh init zsh --config /usr/share/oh-my-posh/themes/gruvbox.omp.json)"' >>~/.zshrc.local
 fi
 
-echo -e "$AZUL Instalação concluída. Reinicie o terminal. $FIM"
+echo -e "$AZUL Instalação concluída. Reinicie o terminal e depois execute zsh_plugins para terminar a configuração do zsh-completions. $FIM"
