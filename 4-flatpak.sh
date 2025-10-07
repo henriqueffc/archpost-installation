c9e1baad3cef29b7ba11719d419149807cc4e53b
Henrique Custódio <henriqueffc17@gmail.com>
2025-10-07 15:02:17 -0300

    inclusão do app flatpak Errands

--- 4-flatpak.sh
+++ 4-flatpak.sh
@@ -59,6 +59,7 @@ flatpak install flathub nl.emphisia.icon -y
 flatpak install flathub com.saivert.pwvucontrol -y
 flatpak install flathub io.missioncenter.MissionCenter -y
 flatpak install flathub be.alexandervanhee.gradia -y
+flatpak install flathub io.github.mrvladus.List -y
 
 # Flathub Remote-Beta
 echo -e "$AZUL Adicionando o remote flathub-beta $FIM"
